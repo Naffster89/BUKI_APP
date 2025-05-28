184e5b0e5603dcde42c51c5c2df28902f0963694
Jairo Hurtado <jahur10@hotmail.com>
2025-05-28 15:36:20 +0900

    user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render "users/show", status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :birthday, :first_name, :last_name, languages: [])
+  end
 end
