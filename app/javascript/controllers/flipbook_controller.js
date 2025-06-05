4b96eeadabaf1deeb796e86639a420f368582916
Naw San <nawsanbmo@gmail.com>
2025-06-05 17:28:46 +0900

    Added languages button in the page

--- app/javascript/controllers/flipbook_controller.js
+++ app/javascript/controllers/flipbook_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["container"]
+  static targets = ["container", 'form']
 
   connect() {
-
     this.pageFlip = new St.PageFlip(this.containerTarget, {
       width: window.screen.width - 50,
       height: window.screen.height,
@@ -16,7 +15,7 @@ export default class extends Controller {
       // 🔽 THIS is what makes it single page
       useMouseEvents: true,
       flippingTime: 700,
-      startPage: 0,
+      startPage: Number(this.element.dataset.page),
       disableFlipByClick: true,
       mode: "single" // ✅ SINGLE page flip mode
     });
@@ -24,7 +23,12 @@ export default class extends Controller {
     this.pageFlip.loadFromHTML(document.querySelectorAll(".my-page"));
     this.pageFlip.on('flip', (e) => {
       // callback code
-      console.log(e.data); // current page number
+      const bookId = Number(this.element.dataset.book)
+      // console.log(e.data); // current page number
+      this.formTarget.action = `/books/${bookId}/pages/${e.data}`
+      window.history.pushState(`page${e.data}`, 'Buki', `/books/${bookId}/pages/${e.data}`);
+
+      // console.log(this.formTarget)
     });
   }
 
